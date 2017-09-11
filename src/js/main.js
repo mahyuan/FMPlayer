8166720e918de76411efdb766c8ed41aa5380a44
mhy-web <dsz9258@qq.com>
2017-09-11 11:41:31 +0800

    My Player

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,7 @@ let Music = function(){
 	this.init()
 	this.bind()
 	this.getChannels()
+	// this.onplay()
 }
 let proto =  Music.prototype
 proto.addEvent = function (node, type, handler) {
@@ -19,7 +20,6 @@ proto.addEvent = function (node, type, handler) {
 		return true;
 	}
 	return false;
-
 }
 
 proto.init = function(){
@@ -27,7 +27,10 @@ proto.init = function(){
 	this.loginbtn = document.querySelector('#loginbtn')
 	this.avatar = document.querySelector('#avatar')
 	this.volumeBtn = document.querySelector('.volume')
+	this.volumeCt = document.querySelector('.volume-pro')
 	this.volumes = document.querySelector('.volume-box-ct')
+	this.volumeBox = document.querySelector('.volume-box')
+	this.volumeBar = document.querySelector('.volume-bar')
 	this.loginCt = document.querySelector('.loginCt')
 	this.thumb = document.querySelector('#thumb-btn')
 	this.collect = document.querySelector('#collect-btn')
@@ -36,8 +39,8 @@ proto.init = function(){
 	this.loop = document.querySelector('#loop')
 	this.extraCt  = document.querySelector('.extra-btn-ct')
 	this.imgCt = document.querySelector('.img-ct')
-	this.lyrcCt = document.querySelector('.lyrcCt')
-	this.lyrc = document.querySelector('.lyrc')
+	this.lyricCt = document.querySelector('.lyricCt')
+	// this.lyric = document.querySelector('.lyric')
 	this.songTitle = document.querySelector('.info-title')
 	this.songArtist = document.querySelector('.info-artist')
 	this.preplay = document.querySelector('#preplay')
@@ -45,11 +48,9 @@ proto.init = function(){
 	this.playCt = document.querySelector('#pause')
 	this.listShowBtn = document.querySelector('#listshow')
 	this.musicList = document.querySelector	('.musicList')
-	this.perc = document.querySelector('#perc')
 
 	this.songCt = document.querySelector('.songCt')
-	this.songChannel = document.querySelector('.songCt>li')
-	// this.songItem =  document.querySelector('.songCt>li')
+	this.songItem = document.querySelector('.channelitem')
 
 	this.imgShow = document.querySelector('#img-show')
 	this.playPre = document.querySelector('#preplay')
@@ -62,19 +63,21 @@ proto.init = function(){
 	this.loopL = document.querySelector('.icon-mulu')
 	this.loopS = document.querySelector('.icon-single')
 
-	this.progress = document.querySelector('.progress')
-	this.barNode = document.querySelector('.progress .bar')
-	this.NowNode = document.querySelector('.progress-now')
+	this.perc = document.querySelector('#perc')
+	this.progressCt = document.querySelector('.progress')
+	this.barNode = document.querySelector('.bar')
+	this.barBox= document.querySelector('.progress-bar')
+	this.nowNode = document.querySelector('.progress-now')
 
 
 	this.curTimeCt = document.querySelector('.curtime')
 	this.maxTimeCt	= document.querySelector('.maxTime')
-
+	this.progressCtWidth = this.progressCt.style.width
 	this.music = document.querySelector('audio')
 
 	this.autoPlay = this.music.autoPlay = true
-	this.loop = this.music.loop=true
-	this.volume = this.music.volume = 1
+	this.loop = this.music.loop = true
+	this.volume = this.music.volume = 0.5
 	this.playing = this.playing = false
 	let timer
 }
@@ -84,28 +87,33 @@ proto.bind = function(){
 	this.addEvent(this.listShowBtn, 'click', function(e){
 		e.stopPropagation();
 		that.musicList.style.bottom = '0';
+		that.volumeCt.classList.remove('vol-active')
 	})
 	this.addEvent(this.container, 'click', function(e){
-		e.stopPropagation();
-		// that.loginCt.style.left = '-62%';
+		// e.stopPropagation();
 		that.musicList.style.bottom = '-42%';
+		that.volumeCt.classList.remove('vol-active')
 	})
 	this.addEvent(this.musicList, 'click', function(e){
 		e.stopPropagation();
 		return;
 	})
-	this.addEvent(this.thumb, 'click', function(){
+	this.addEvent(this.thumb, 'click', function(e){
+		e.stopPropagation();
 		that.thumb.classList.toggle('extra-active')
 	})
-	this.addEvent(this.collect, 'click', function(){
+	this.addEvent(this.collect, 'click', function(e){
+		e.stopPropagation();
 		that.collect.classList.toggle('extra-active')
 	})
 
 	this.addEvent(this.volumeBtn, 'click', function(e){
-		that.volumes.classList.toggle('vol-active')
+		e.stopPropagation();
+		that.volumeCt.classList.toggle('vol-active')
 	})
 
 	this.addEvent(this.loopbtn, 'click', function(e){
+		e.stopPropagation();
 		if(that.loop == true){
 			that.loop = false
 			that.loopS.classList.add('active')
@@ -116,16 +124,18 @@ proto.bind = function(){
 			that.loopL.classList.add('active')
 		}
 	})
-	this.addEvent(this.songChannel, 'click', function(e){
-		if (e.target.tagName.toLowerCase() != "li" || !e.target.hasAttributes("channel_id")) return;
-		that.channel_id = e.target.getAttribute('data-channel-id')
-		that.getSong()
-		console.log('要获取的歌的id：' +channelId )
+	this.addEvent(this.songCt, 'click', function(e){
+		e.stopPropagation();
+		if (e.target.tagName.toLowerCase() != "li") return;
+		that.getSong(that.channelId)
 	})
 	this.addEvent(this.playCt, 'click', function(e){
+		e.stopPropagation();
+
 		if(that.playing == true){
 			that.music.pause()
 			that.playing = false
+			console.log("that.playing == true"+ that.playing)
 			that.pauseIcon.classList.add('active')
 			that.playIcon.classList.remove('active')
 			that.imgCt.classList.remove('img-rotate')
@@ -139,29 +149,68 @@ proto.bind = function(){
 	})
 
 	this.addEvent(this.playPre,'click',function(e){
+		e.stopPropagation();
 		that.getSong()
+		// that.playing = true
+		that.pauseIcon.classList.remove('active')
+		that.playIcon.classList.add('active')
+		that.imgCt.classList.add('img-rotate')
 	})
 	this.addEvent(this.playNext, 'click',function(e){
+		e.stopPropagation();
 		that.getSong()
-	})
-	this.addEvent(this.music ,'loadedmetadata',function(){
-		that.timeRender()
-	})
-	this.addEvent(this.music ,'timeupdate',function(){
-		that.progressRender()
-	})
-	this.addEvent(this.music, 'loadedmetadata', function(){
-		that.progressRender()
+		// that.playing = true
+		that.pauseIcon.classList.remove('active')
+		that.playIcon.classList.add('active')
+		that.imgCt.classList.add('img-rotate')
 	})
 
-	this.addEvent(this.music, 'timeupdate', function(){
+	this.addEvent(this.music ,'loadedmetadata',function(e){
+		e.stopPropagation();
 		that.timeRender()
 	})
+	this.addEvent(this.music ,'timeupdate',function(e){
+		e.stopPropagation();
+		that.progressRender()
+	})
+	this.addEvent(this.music, 'loadedmetadata', function(e){
+		e.stopPropagation();
+		that.progressRender()
+	})
+	this.addEvent(this.music, 'timeupdate', function(e){
+		e.stopPropagation();
+		that.timeRender()
 
-	this.addEvent(this.barNode, 'click', function(){
-		var percent = e.offsetX/parseInt(getComputedStyle(this).width)
+		// let liH = $(".lyric li").eq(1).outerHeight() - 3;             //每行高度
+		// for (var i = 0; i < lyric.length; i++) {                          //遍历歌词下所有的li
+		// 	var curT = $(".lyric li").eq(i).attr("dataTime");      //获取当前li存入的当前一排歌词时间
+		// 	var nexT = $(".lyric li").eq(i + 1).attr("dataTime");
+		// 	var curTime = that.myAudio.currentTime;
+		// 	if ((curTime > curT) && (curT < nexT)) {                //当前时间在下一句时间和歌曲当前时间之间的时候 就渲染 并滚动
+		// 		$(".lyric li").removeClass("lyric-active");
+		// 		$(".lyric li").eq(i).addClass("lyric-active");
+		// 		$('.lyric ul').css({
+		// 			'top': -liH * (i - 2),
+		// 			"transition": "1s"
+		// 		});
+		// 	}
+		// }
+
+	})
+
+	this.addEvent(this.barBox, 'click', function(e){
+		e.stopPropagation();
+		let percent = e.offsetX/parseInt(getComputedStyle(this).width)
 		that.music.currentTime = percent * that.music.duration
-		that.NowNode.style.width = percent*100+"%"
+		that.barNode.style.width = percent*100+"%"
+	})
+	this.addEvent(this.volumeBar, 'click', function(e){
+		e.stopPropagation();
+		console.log("percent :")
+		let percent = 1 - (e.offsetY/parseInt(getComputedStyle(this).height))
+		console.log(percent)
+		that.music.volume = percent
+		that.volumeBox.style.height = percent*100+"%"
 	})
 }
 
@@ -188,19 +237,11 @@ proto.getSong = function(){
 		that.imgShow.setAttribute('src', songImg)
 		that.songArtist.innerText = songArtist
 		that.songTitle.innerText = songTitle
-
-
-								that.music.playSong()
-								that.lyrc.empty()
-								that.getLyric()
+		that.playing = true
 
 	}).fail(function(){
 		that.getSong()
 	})
-
-
-
-
 }
 proto.getChannels = function(){
 	let that = this
@@ -212,6 +253,12 @@ proto.getChannels = function(){
 		console.log(response)
 		let channels = response.channels
 		that.renderChanels(channels)
+		that.getSong(channels[0])
+		that.playing = true
+		that.pauseIcon.classList.remove('active')
+		that.playIcon.classList.add('active')
+		that.imgCt.classList.add('img-rotate')
+
 	}).fail(function(){
 		that.songCt.innerText('获取清单失败')
 	})
@@ -224,39 +271,62 @@ proto.getLyric = function(){
 		method: 'get',
 		dataType: 'json',
 		data: {sid : songId}, //是否可以获取到该变量，请核对
-
 	}).done(function(ret){
 		that.renderLyric(ret.lyric)
 	}).dail(function(){
-		that.lyrc.innerText = "这首歌没有歌词！"
+		that.lyric.innerText = "这首歌没有歌词！"
 	})
 }
 proto.playSong = function(){
-
+	this.music.play()
 }
-proto.renderLyric = function(){
+proto.renderLyric = function(e){  //解析歌词,返回一个二维数组
+	let that = this
+	let lines = e.split("\n")
+	let pattern = /^\[\d{2}\:\d{2}\.\d{2}\]/
+	let lyricArr = []
+	let item = ""
+	lines.forEach(function(i){
+		if(!pattern.test(i)){  //剔除收到数据中没有时间的部分
+			lines.splice(i,1)
+			return
+		}
+		let time = i.match(pattern) //把歌词分为：时间和歌词两个部分
+		let lyric = i.split(time)
+		let seconds = time[0][1]*600 + time[0][2]*10 + time[0][5]*1  //将时间换算为秒
+		lyricArr.push([seconds, lyric[1]])   //将整个歌词保存至二维数组中，形式为[时间，歌词]；
+	})
 
+	lyricArr.forEach(function(i){
+		item += '<li dataTime ="'+i[0]+'">'+i[1]+'</li>'
+	})
+	this.lyricCt.innerHTML = item
 }
-
-
 
 proto.renderChanels = function(channels){
 	const html = channels.map(function(channel){
-		return '<li data-channel_id="'+channel.channel_id + '">' + channel.name + '</li>'
+		return '<li class="channelitem" data-channel_id="'+channel.channel_id + '">' + channel.name + '</li>'
 	}).join('')
 	this.songCt.innerHTML = html
 }
-proto.songRender = function(title, artist, lyrc){
+proto.songRender = function(title, artist, lyric){
 	this.songTitle.append(title)
 	this.songArtist.append(artist)
 
-							this.lyrcCt.append(lyrc)
-							//歌词要分解开时间
+							// this.lyricCt.append(lyric)
+
 }
 
 
 proto.progressRender = function (){
+	var that = this
 	let curTime = parseInt(this.music.currentTime)
+	let mDuration = parseInt(this.music.duration)
+	let currentTime = parseInt(this.music.currentTime);
+
+	let percent = (currentTime/mDuration)*100+'%'
+
+	this.barNode.style.width = percent
 	let minute = "00"
 	if(curTime<10){
 		curTime = "0" + curTime
@@ -270,16 +340,23 @@ proto.progressRender = function (){
 			curTime = "0" + curTime
 		}
 	}
-
 	this.curTimeCt.innerHTML = minute + ':' + curTime
+
+	// let minutes = parseInt(this.music.currentTime/60)
+	// let seconds = parseInt(this.music.currentTime%60)+''
+	// seconds = seconds.length == 2? seconds : '0'+seconds
+	// this.maxTimeCt.innerHTML = minutes+ ':' +seconds
+
 }
 
 proto.timeRender = function(){
+
+
 	let mDuration = parseInt(this.music.duration)
 	let minute = "00"
-	let maxTime = this.progress.getAttribute('max')
+
 	if(mDuration < 10){
-		mDuration = "0" +mDuration
+		mDuration = "0" + mDuration
 	}
 	if(mDuration>60){
 		minute = parseInt(mDuration/60)
@@ -294,24 +371,22 @@ proto.timeRender = function(){
 	this.maxTimeCt.innerHTML = minute+ ':' + mDuration
 }
 
-proto.updateProgress = function(){
-	let percent = (this.music.currentTime/this.music.duration)*100+'%'
-	progressNowNode.style.width = percent
-
-	let minutes = parseInt(this.music.currentTime/60)
-	let seconds = parseInt(this.music.currentTime%60)+''
-	seconds = seconds.length == 2? seconds : '0'+seconds
-	this.curTimeCt.innerText = minutes + ':' + seconds
-}
-proto.onplaying = function(){
+proto.onplay = function(){
+	let that = this
+	let percent = currentTime/mDuration*100
 	this.timer = setInterval(function(){
-		updateProgress()
+		that.updateProgress()
 	}, 1000)
-	console.log('play')
+	if( this.loop == true && percent == 1){  //自动切歌
+		console.log('loop:'+ this.loop)
+		console.log("percent+percent")
+		that.getSong();
+		console.log('getsong?')
+	}
 }
+
 proto.onpause = function(){
 	console.log('pause')
 	clearInterval(timer)
 }
-
 new Music(document.querySelector('.container'))
