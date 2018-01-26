635aff0c7db9252caed61bbbbb29cab34162d386
mhyuan <dsz9258@163.com>
2018-01-27 01:49:30 +0800

    fix bug about max time show

--- src/js/main.js
+++ src/js/main.js
@@ -328,28 +328,13 @@ proto.progressRender = function (){
 	let percent = (currentTime/mDuration)*100+'%'
 
 	this.barNode.style.width = percent
-	let minutes = parseInt(curTime/60%60) || 0
+	let minutes = parseInt(curTime/60) || 0
 	let seconds = parseInt(curTime%60)
 
 	minutes = (minutes <10 ? '0' : '') + minutes
 	seconds = (seconds <10 ? '0' : '') + seconds
-	/*
-	if(curTime<10){
-		curTime = "0" + curTime
-	}
-	if(curTime>60){
-		minute = parseInt(curTime%60)
-		if(minute<10){
-			minute = "0" + minute
-		}
-		if(curTime<10){
-			curTime = "0" + curTime
-		}
-	}*/
 
-	// console.log()
-// console.log(minutes, seconds)
-	this.curTimeCt.innerHTML = `${minutes}:${seconds} `
+	this.curTimeCt.innerHTML = `${minutes}:${seconds}`
 
 	// let minutes = parseInt(this.music.currentTime/60)
 	// let seconds = parseInt(this.music.currentTime%60)+''
@@ -360,24 +345,14 @@ proto.progressRender = function (){
 
 proto.timeRender = function(){
 
+	let mDuration = parseInt(this.music.duration) || 0
+	let minutes = parseInt(mDuration/60)
+	let seconds = parseInt(mDuration%60)
 
-	let mDuration = parseInt(this.music.duration)
-	let minute = "00"
-
-	if(mDuration < 10){
-		mDuration = "0" + mDuration
-	}
-	if(mDuration>60){
-		minute = parseInt(mDuration/60)
-		mDuration = parseInt(mDuration%60)
-		if(minute<10){
-			minute = "0" + minute
-		}
-		if(mDuration<10){
-			mDuration = "0" + mDuration
-		}
-	}
-	this.maxTimeCt.innerHTML = minute+ ':' + mDuration
+	minutes = (minutes < 10 ? '0' : '') + minutes
+	seconds = (seconds < 10 ? '0' : '') + seconds
+ 	
+	this.maxTimeCt.innerHTML = `${minutes}:${seconds}`
 }
 
 proto.onplay = function(){
